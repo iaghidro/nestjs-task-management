6a46b2e867c5c789259eaf54e34f5e124bb96f54
Steve Ghidro <iaghidro@gmail.com>
2020-11-15 12:24:49 -0500

    add password encryption using generated salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
     try {
       await user.save();
     } catch (error) {
@@ -22,5 +25,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string) {
+    return bcrypt.hash(password, salt);
   }
 }
